89a9e4d7b14743caf0e7f414af7bd950fd5bd1e8
amit <akviitian@gmail.com>
2024-01-23 02:46:07 +0530

    Updates

---
+++ precache-manifest.d67c05f843e61b0b98f3ed442e642ad8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8837b35df01ead0d7859e1c90e3e2ec",
+    "url": "/amitvish/Garage-App-Frontend/index.html"
+  },
+  {
+    "revision": "ce884910e8dfd42d7b0e",
+    "url": "/amitvish/Garage-App-Frontend/static/css/2.a49aa9f3.chunk.css"
+  },
+  {
+    "revision": "427b528a8b73b508294c",
+    "url": "/amitvish/Garage-App-Frontend/static/css/main.5a6f32d2.chunk.css"
+  },
+  {
+    "revision": "ce884910e8dfd42d7b0e",
+    "url": "/amitvish/Garage-App-Frontend/static/js/2.42a64114.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/amitvish/Garage-App-Frontend/static/js/2.42a64114.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "427b528a8b73b508294c",
+    "url": "/amitvish/Garage-App-Frontend/static/js/main.54b9bcf0.chunk.js"
+  },
+  {
+    "revision": "ce05285204212ed824fd",
+    "url": "/amitvish/Garage-App-Frontend/static/js/runtime-main.534f1368.js"
+  }
+]);
